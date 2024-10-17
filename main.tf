6cca5a58610d60214d1e23d986e42353a8ef8132
Ram Bharath <mrambharath7@gmail.com>
2024-10-17 22:22:52 +1100

    rg

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "inlogik_rg" {
+  name     = "inlogik_rg"
+  location = "East US"
+}
